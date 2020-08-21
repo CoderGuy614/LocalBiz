e01e0598b492043d8e73e488acf4e2064498b679
CoderGuy614 <jlutz.110@gmail.com>
2020-08-21 17:22:30 +0800

    Able to update user name and password but new password is unhashed

--- controllers/user.js
+++ controllers/user.js
@@ -17,3 +17,39 @@ exports.read = (req, res) => {
   req.profile.password = undefined;
   return res.json(req.profile);
 };
+
+exports.update = (req, res) => {
+  const { name, password } = req.body;
+  User.findOne({ _id: req.profile._id }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+    if (!name) {
+      return res.status(400).json({
+        error: "Name is required",
+      });
+    } else {
+      user.name = name;
+    }
+    if (password) {
+      if (password.length < 6) {
+        return res.status(400).json({
+          error: "Password Must be At Least 6 Characters",
+        });
+      } else {
+        user.password = password;
+      }
+    }
+    user.save((err, updatedUser) => {
+      if (err) {
+        console.log("Sorry, we couldn't update your profile", err);
+        return res.status(400).json({
+          error: "User Update Failed",
+        });
+      }
+      res.json(updatedUser);
+    });
+  });
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,10 +5,16 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const auth = require("../../middleware/auth");
-const { userById, read } = require("../../controllers/user");
+const { userById, read, update } = require("../../controllers/user");
 
 // Get a registered user by their ID
 router.get("/users/:userId", read);
+
+//Update a User
+router.put("/users/:userId", update);
+
+// //Delete a User
+// router.delete("/user/:userId", remove);
 
 router.param("userId", userById);
 
