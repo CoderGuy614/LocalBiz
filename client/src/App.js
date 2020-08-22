d6199e7eb7fea054dbf5e3f3e08f60ec5c63ac44
CoderGuy614 <jlutz.110@gmail.com>
2020-08-22 12:13:22 +0800

    Install Bootstrap and Sass, Customize color scheme

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-
-import "./App.css";
+import "./App.scss";
+import { Button } from "react-bootstrap";
 
 function App() {
-  return <div className="App">Hello From React</div>;
+  return (
+    <div className="App">
+      <Button variant="primary">Click ME</Button>
+      <Button variant="secondary">Click ME</Button>
+      <Button variant="success">Click ME</Button>
+      <Button variant="info">Click ME</Button>
+      <Button variant="warning">Click ME</Button>
+      <Button variant="danger">Click ME</Button>
+    </div>
+  );
 }
 
 export default App;
