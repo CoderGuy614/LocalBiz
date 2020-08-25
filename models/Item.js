e4ac5df96ed1dc691b0c7531656939312c3fb645
CoderGuy614 <jlutz.110@gmail.com>
2020-08-25 14:03:21 +0800

    Modify Item Model

--- models/Item.js
+++ models/Item.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 module.exports = mongoose.model("items", {
   name: {
     type: String,
+    trim: true,
+    maxlength: 32,
     required: [true, "name is required"],
   },
   description: {
     type: String,
+    trim: true,
+    maxlength: 2000,
     required: [true, "description is required"],
   },
   inStock: {
@@ -15,10 +19,25 @@ module.exports = mongoose.model("items", {
   },
   price: {
     type: Number,
+    trim: true,
+    maxlength: 32,
     required: [true, "price is required"],
   },
   canDeliver: {
     type: Boolean,
     default: false,
+  },
+  category: {
+    type: ObjectId,
+    ref: "Category",
+  },
+  business: {
+    type: ObjectId,
+    ref: "Biz",
+    required: true,
+  },
+  photo: {
+    data: Buffer,
+    contentType: String,
   },
 });
