770cfcc52ea0bd5612eab51c8022d4697d2c971e
CoderGuy614 <jlutz.110@gmail.com>
2020-08-30 10:34:50 +0800

    Refactor Hours Input Form

--- client/src/components/core/HoursForm.js
+++ client/src/components/core/HoursForm.js
@@ -3,47 +3,71 @@ import { Container, Button, Table } from "react-bootstrap";
 import Layout from "./Layout";
 import HourInputRow from "./HourInputRow";
 const HoursForm = () => {
-  const [openTimes, setOpenTimes] = useState({
-    Monday: "12:00 AM",
-    Tuesday: "12:00 AM",
-    Wednesday: "12:00 AM",
-    Thursday: "12:00 AM",
-    Friday: "12:00 AM",
-    Saturday: "12:00 AM",
-    Sunday: "12:00 AM",
-  });
-
-  const [closeTimes, setCloseTimes] = useState({
-    Monday: "12:00 AM",
-    Tuesday: "12:00 AM",
-    Wednesday: "12:00 AM",
-    Thursday: "12:00 AM",
-    Friday: "12:00 AM",
-    Saturday: "12:00 AM",
-    Sunday: "12:00 AM",
-  });
-
-  const [isClosed, setIsClosed] = useState({
-    Monday: false,
-    Tuesday: false,
-    Wednesday: false,
-    Thursday: false,
-    Friday: false,
-    Saturday: false,
-    Sunday: false,
+  const [values, setValues] = useState({
+    Monday: {
+      open: "12:00 AM",
+      close: "12:00 AM",
+      isClosed: false,
+    },
+    Tuesday: {
+      open: "12:00 AM",
+      close: "12:00 AM",
+      isClosed: false,
+    },
+    Wednesday: {
+      open: "12:00 AM",
+      close: "12:00 AM",
+      isClosed: false,
+    },
+    Thursday: {
+      open: "12:00 AM",
+      close: "12:00 AM",
+      isClosed: false,
+    },
+    Friday: {
+      open: "12:00 AM",
+      close: "12:00 AM",
+      isClosed: false,
+    },
+    Saturday: {
+      open: "12:00 AM",
+      close: "12:00 AM",
+      isClosed: false,
+    },
+    Sunday: {
+      open: "12:00 AM",
+      close: "12:00 AM",
+      isClosed: false,
+    },
   });
 
   const handleTimeChange = (day, openOrClose) => (e) => {
     if (openOrClose === "open") {
-      setOpenTimes({ ...openTimes, [day]: e.target.value });
-    } else {
-      setCloseTimes({ ...closeTimes, [day]: e.target.value });
+      const prevState = values[day];
+      prevState.open = e.target.value;
+      setValues({ ...values, [day]: prevState });
+    } else if (openOrClose === "close") {
+      const prevState = values[day];
+      prevState.close = e.target.value;
+      setValues({ ...values, [day]: prevState });
     }
   };
 
   const handleIsClosed = (e, day) => {
-    setIsClosed({ ...isClosed, [day]: e.target.checked });
+    const prevState = values[day];
+    prevState.isClosed = e.target.checked;
+    setValues({ ...values, [day]: prevState });
   };
+
+  const daysArray = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
   return (
     <Layout
@@ -61,62 +85,17 @@ const HoursForm = () => {
             </tr>
           </thead>
           <tbody>
-            <HourInputRow
-              day="Monday"
-              openTime={openTimes.Monday}
-              closeTime={closeTimes.Monday}
-              isClosed={isClosed.Monday}
-              handleIsClosed={handleIsClosed}
-              handleTimeChange={handleTimeChange}
-            />
-            <HourInputRow
-              day="Tuesday"
-              openTime={openTimes.Tuesday}
-              closeTime={closeTimes.Tuesday}
-              isClosed={isClosed.Tuesday}
-              handleIsClosed={handleIsClosed}
-              handleTimeChange={handleTimeChange}
-            />
-            <HourInputRow
-              day="Wednesday"
-              openTime={openTimes.Wednesday}
-              closeTime={closeTimes.Wednesday}
-              isClosed={isClosed.Wednesday}
-              handleIsClosed={handleIsClosed}
-              handleTimeChange={handleTimeChange}
-            />
-            <HourInputRow
-              day="Thursday"
-              openTime={openTimes.Thursday}
-              closeTime={closeTimes.Thursday}
-              isClosed={isClosed.Thursday}
-              handleIsClosed={handleIsClosed}
-              handleTimeChange={handleTimeChange}
-            />
-            <HourInputRow
-              day="Friday"
-              openTime={openTimes.Friday}
-              closeTime={closeTimes.Friday}
-              isClosed={isClosed.Friday}
-              handleIsClosed={handleIsClosed}
-              handleTimeChange={handleTimeChange}
-            />
-            <HourInputRow
-              day="Saturday"
-              openTime={openTimes.Saturday}
-              closeTime={closeTimes.Saturday}
-              isClosed={isClosed.Saturday}
-              handleIsClosed={handleIsClosed}
-              handleTimeChange={handleTimeChange}
-            />
-            <HourInputRow
-              day="Sunday"
-              openTime={openTimes.Sunday}
-              closeTime={closeTimes.Sunday}
-              isClosed={isClosed.Sunday}
-              handleIsClosed={handleIsClosed}
-              handleTimeChange={handleTimeChange}
-            />
+            {daysArray.map((d, i) => (
+              <HourInputRow
+                key={i}
+                day={d}
+                openTime={values[d].open}
+                closeTime={values[d].close}
+                isClosed={values[d].isClosed}
+                handleIsClosed={handleIsClosed}
+                handleTimeChange={handleTimeChange}
+              />
+            ))}
           </tbody>
         </Table>
         <Button block>Save Changes</Button>
