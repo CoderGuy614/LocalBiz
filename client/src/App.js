80c1acbb189d2715aa5dea4e4a9276e425a1c6fd
CoderGuy614 <jlutz.110@gmail.com>
2020-09-17 21:18:15 +0800

    Modify AppJS biz route

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ const App = () => {
         <Navigation />
         <Switch>
           <Route exact path="/" component={Shop}></Route>
-          <Route exact path="/biz/:bizId" component={Biz}></Route>
+          <Route exact path="/:bizId" component={Biz}></Route>
           <Route exact path="/post/biz" component={AddBizForm}></Route>
           <Route exact path="/post/hours/:bizId" component={HoursForm}></Route>
           <Route exact path="/signup" component={Signup}></Route>
