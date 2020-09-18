a87ce077df2126868810954709ed0c875275b3e2
CoderGuy614 <jlutz.110@gmail.com>
2020-09-18 15:55:08 +0800

    Deploy successful and working

--- server.js
+++ server.js
@@ -41,12 +41,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-
-  // app.use(express.static(path.join(__dirname, "build")));
-
-  // app.get("/*", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "build", "index.html"));
-  // });
 }
 
 const PORT = process.env.PORT || 5000;
