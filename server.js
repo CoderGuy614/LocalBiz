bad26fed99dc2673d4eb7547bb86f021b676d3b7
Jonathan Lutz <jlutz.110@gmail.com>
2025-05-25 21:21:52 -0700

    update cors

--- server.js
+++ server.js
@@ -26,29 +26,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Replace your current CORS configuration with this
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'https://master.d28l3w9jtysc89.amplifyapp.com',
-      'https://d28l3w9jtysc89.amplifyapp.com'
-    ];
-    
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'X-Auth-Token', 'Authorization']
-};
+
+app.use(cors({
+  origin: true, // This allows all origins
+  credentials: false
+}));
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 
 app.use("/api", userRoutes);
